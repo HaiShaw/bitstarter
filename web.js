0b6d6e613317783ec2b67feb5ef71d5f73a33a84
HaiShaw <hixiao@gmail.com>
2013-07-11 06:09:08 +0000

    ...

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var datastr;
+
+fs.readFileSync('index.html', function(err, data) {
+  if (err) throw err;
+  datastr = data;
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(datastr);
 });
 
 var port = process.env.PORT || 5000;
